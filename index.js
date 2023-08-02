da3b19c98e2e3f39fbc849d241dcc1531bc51497
jgjfuture <abovethechicken@outlook.jp>
2023-08-02 18:36:42 +0000

    move async position

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ import { alalyzeImage } from './analyze.js';
 import { publishMessage, makePublishData } from './publish.js';
 import { topicName } from './pubsub.js';
 
-functions.cloudEvent('entoryPoint', cloudEvent => {
-    (async () => {
-        const base64Message = cloudEvent.data.message.data;
-        if (!base64Message) {
-            console.log('No message received!');
-            return;
-        }
-        const messageString = Buffer.from(base64Message, 'base64').toString();
-        const message = JSON.parse(messageString);
-        const imageURL = message.imageURL;
-        const pageId = message.notionPageId;
-        const reasoningText = await alalyzeImage(imageURL);
-        await publishMessage(topicName, makePublishData(pageId, reasoningText));
-    })();
+functions.cloudEvent('entoryPoint', async cloudEvent => {
+    const base64Message = cloudEvent.data.message.data;
+    if (!base64Message) {
+        console.log('No message received!');
+        return;
+    }
+    const messageString = Buffer.from(base64Message, 'base64').toString();
+    const message = JSON.parse(messageString);
+    const imageURL = message.imageURL;
+    const pageId = message.notionPageId;
+    const reasoningText = await alalyzeImage(imageURL);
+    await publishMessage(topicName, makePublishData(pageId, reasoningText));
 });
